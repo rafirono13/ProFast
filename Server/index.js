05a3b3d2df51455679f6c1d1ea4690289b45e43c
rafirono13 <farhanmahbubrafi@gmail.com>
2025-06-28 23:06:33 +0600

    intial setup done.

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,9 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-/* // ------------------ MONGODB SETUP (Cmd+K, Cmd+U to uncomment) ------------------
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = process.env.MONGO_URI;
 
@@ -20,33 +18,29 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // await client.connect(); // Connect to MongoDB on startup
+    await client.connect();
 
     // --- Start Your API Endpoints Here ---
-    
-    // Example: app.get('/users', async (req, res) => { ... });
 
     // --- End Your API Endpoints Here ---
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    await client.close();
   }
 }
+
 run().catch(console.dir);
 
-// -------------------------------------------------------------------------- 
-*/
-
-
-app.get('/', (req, res) => {
-  res.send('API Server is running...');
+app.get("/", (req, res) => {
+  res.send("API Server is running...");
 });
 
 app.listen(port, () => {
